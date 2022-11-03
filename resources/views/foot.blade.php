cb7111de11ce7b81763476c12e4ae266c5b0b908
unknown <ekin.tanuwijaya@gmail.com>
2022-11-03 19:11:30 +0700

    GSLC-2

--- resources/views/foot.blade.php
+++ resources/views/foot.blade.php
@@ -1,11 +1,11 @@
 <footer>
     <div class="text-center bg-success text-white p-2">
         <div class="d-flex justify-content-center">
-            <h4 >GSLC</h4><h4>-1</h4>
+            <h4 >GSLC</h4>
         </div>
         <div>
             <div class="d-flex justify-content-center p-1">
-                <b>GSLC-1</b>
+                <b>GSLC</b>
                 &nbspis a Website that contains an answer of my forum
             </div>
         </div>
